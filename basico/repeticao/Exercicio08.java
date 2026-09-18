0b098d0c8e45fc482ae4389a2549c486b18ff4e7
Gabriel Dos Santos Nascimento <gabriel09santos@gmail.com>
2026-09-18 19:55:10 -0300

    Update Exercicio08.java

--- basico/repeticao/Exercicio08.java
+++ basico/repeticao/Exercicio08.java
@@ -1,4 +1,19 @@
 package flamingo.aprendendo.basico.repeticao;
 
 public class Exercicio08 {
+  public static void main(String[] args) {
+        int quantidadeProdutos = 5;
+        double valorProduto = 20.0;
+        double totalCompra = 0.0;
+
+
+        for (int i = 0; i < quantidadeProdutos; i++) {
+            totalCompra += valorProduto;
+        }
+
+       
+        System.out.println("Quantidade de produtos: " + quantidadeProdutos);
+        System.out.println("Valor de cada produto: R$ " + valorProduto);
+        System.out.println("Total da compra: R$ " + totalCompra);
+    }
 }
